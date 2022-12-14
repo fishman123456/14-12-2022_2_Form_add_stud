2b4a5c2338db0410975f03140670c17a5d0d3026
profi <profi@academtop.local>
2022-12-14 20:46:57 +0700

    Initial commit

---
+++ 14-12-2022_2_Form_add_stud/Form1.cs
@@ -0,0 +1,21 @@
+namespace _14_12_2022_2_Form_add_stud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class Student 
+        {
+        public string? _name;
+        public string? _soneme;
+        public 
+    }
+}
---
+++ 14-12-2022_2_Form_add_stud/Program.cs
@@ -0,0 +1,23 @@
+namespace _14_12_2022_2_Form_add_stud
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
+// добавить три поля
+// 1 Фамилия - label, textbox
+// 2 Имя
+// 3 Группа  - ComboBox
+// 4 Дата выбор при помощи календаря
+// 
